67f0f6557fb5acdfe710a62b75a0040079665074
Štěpán Beran <beranstepan@icloud.com>
2025-04-09 10:24:36 +0200

    [energymanager] handle events asynchronously

--- bundles/org.openhab.binding.energymanager/src/main/java/org/openhab/binding/energymanager/internal/handler/EnergyManagerEventSubscriber.java
+++ bundles/org.openhab.binding.energymanager/src/main/java/org/openhab/binding/energymanager/internal/handler/EnergyManagerEventSubscriber.java
@@ -12,35 +12,56 @@
  */
 package org.openhab.binding.energymanager.internal.handler;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.function.BiConsumer;
+import java.util.stream.Stream;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 import org.openhab.binding.energymanager.internal.enums.InputStateItem;
 import org.openhab.binding.energymanager.internal.handler.util.ConsumerMetadata;
-import org.openhab.core.events.Event;
 import org.openhab.core.events.EventFilter;
 import org.openhab.core.events.EventSubscriber;
 import org.openhab.core.items.events.AbstractItemEventSubscriber;
 import org.openhab.core.items.events.ItemStateEvent;
+import org.openhab.core.model.script.actions.Exec;
 import org.openhab.core.thing.ThingUID;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * The {@link EnergyManagerEventSubscriber} handles the changes in items of all EnergyManager thing instances
- * and calls their appropriate callback functions.
+ * and calls their appropriate callback functions in separate threads.
  *
  * @author <Štěpán Beran> - Initial contribution
  */
 @NonNullByDefault
-@Component(service = { EnergyManagerEventSubscriber.class, EventSubscriber.class })
+@Component(service = {EnergyManagerEventSubscriber.class, EventSubscriber.class})
 public class EnergyManagerEventSubscriber extends AbstractItemEventSubscriber {
+    private static final Logger log = LoggerFactory.getLogger(EnergyManagerEventSubscriber.class);
 
-    Map<String, Set<ConsumerMetadata>> eventConsumers = new ConcurrentHashMap<>();
+
+    private final Map<String, Set<ConsumerMetadata>> eventConsumers;
+    private final ExecutorService executorService;
     final Object lock = new Object();
+
+
+    @Activate
+    public EnergyManagerEventSubscriber() {
+        eventConsumers = new ConcurrentHashMap<>();
+        // using virtual threads because of their lightweight nature
+        executorService = Executors.newVirtualThreadPerTaskExecutor();
+        log.trace("Event manager Initialized");
+    }
 
     @Override
     public @Nullable EventFilter getEventFilter() {
@@ -49,14 +70,23 @@ public class EnergyManagerEventSubscriber extends AbstractItemEventSubscriber {
 
     @Override
     protected void receiveUpdate(ItemStateEvent updateEvent) {
-        if (eventConsumers.containsKey(updateEvent.getItemName())) {
-            eventConsumers.get(updateEvent.getItemName())
-                    .forEach(it -> it.consumer().accept(it.inputStateItem(), updateEvent));
+        var consumers = eventConsumers.get(updateEvent.getItemName());
+        if (consumers != null) {
+            consumers.forEach(consumerMetadata -> {
+                // Execution of consumers in background threads so that we don't block the event bus
+                executorService.submit(() -> {
+                    try {
+                        consumerMetadata.consumer().accept(consumerMetadata.inputStateItem(), updateEvent);
+                    } catch (Exception e) {
+                        log.error(e.getMessage(), e);
+                    }
+                });
+            });
         }
     }
 
     public void registerEventsFor(ThingUID thingUID, Map<String, InputStateItem> itemMapping,
-            BiConsumer<InputStateItem, ItemStateEvent> consumer) {
+                                  BiConsumer<InputStateItem, ItemStateEvent> consumer) {
         synchronized (lock) {
             itemMapping.keySet()
                     .forEach(itemName -> eventConsumers.computeIfAbsent(itemName, k -> ConcurrentHashMap.newKeySet())
@@ -69,5 +99,11 @@ public class EnergyManagerEventSubscriber extends AbstractItemEventSubscriber {
             // assuming there will not be many instances, therefore linear complexity is sufficient
             eventConsumers.forEach((key, value) -> value.removeIf(v -> v.thingUID().equals(thingUID)));
         }
+    }
+
+    @Deactivate
+    public void deactivate(){
+        executorService.shutdown();
+        log.trace("Event manager disposed");
     }
 }
